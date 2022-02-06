c1f62febb807a1d57867e2002e08b0154a1f3bd4
kawabata <s1931115@s.konan-u.ac.jp>
2022-02-06 16:53:26 +0900

    [UPDATE]15Promiseを使用する

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -3,14 +3,31 @@ async function getData() {
   // ３秒後にフルネームのリストを表示されるようにしましょう。
   // 最初から表示されていると、クリアになりません。
   const userList = [
-    { id: 1, first_name: '優', family_name: '大木', affilication: 'TechTrain', is_student: false },
-    { id: 2, first_name: '太郎', family_name: '山田', affilication: 'HogeHoge大学', is_student: true }
+    {
+      id: 1,
+      first_name: "優",
+      family_name: "大木",
+      affilication: "TechTrain",
+      is_student: false,
+    },
+    {
+      id: 2,
+      first_name: "太郎",
+      family_name: "山田",
+      affilication: "HogeHoge大学",
+      is_student: true,
+    },
   ];
-  const result = await test()
-  return await result
+  await test(3);
+  const result = userList.map((element) => {
+    element.full_name = `${element.family_name} ${element.first_name}`;
+    return element;
+  });
+  return await result;
 }
 
-function test() {
-  return
+function test(n) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, n * 1000);
+  });
 }
-
